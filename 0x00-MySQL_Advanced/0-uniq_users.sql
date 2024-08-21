b8699acf5ce8d0cbed901b1b27a2c8c04f364212
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-08-21 19:10:44 +0100

    updated

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- script that creates a table users
 
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	name VARCHAR(255)
