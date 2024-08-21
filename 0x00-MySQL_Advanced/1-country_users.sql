4f0d4d925f1761833e3d816e78dee98149bd7671
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-08-21 18:08:40 +0100

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- script that creates a table users
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
