541cb0c2a910596faf27e9e289c8d6fdc4a9e700
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-08-30 22:01:46 +0100

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -47,6 +47,31 @@ def call_history(method: Callable) -> Callable:
         return result
     return wrapper
 
+def replay(fn: Callable) -> None:
+    '''
+    Displays the call history of a Cache class' method.
+    '''
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
+
 
 class Cache:
     def __init__(self) -> None:
@@ -121,26 +146,3 @@ class Cache:
                 'inputs': inputs,
                 'outputs': outputs
                 }
-
-def replay(method: Callable):
-    """
-    Display the history of calls of a particular function.
-
-    Args:
-        method (Callable): The method whose history is to be replayed.
-    """
-    redis_instance = method.__self__._redis
-
-    input_key = f"{method.__qualname__}:inputs"
-    output_key = f"{method.__qualname__}:outputs"
-
-    # Fetch the inputs and outputs from Redis
-    inputs = redis_instance.lrange(input_key, 0, -1)
-    outputs = redis_instance.lrange(output_key, 0, -1)
-
-    print(f"{method.__qualname__} was called {len(inputs)} times:")
-
-    for inp, outp in zip(inputs, outputs):
-        inp = inp.decode('utf-8')
-        outp = outp.decode('utf-8')
-        print(f"{method.__qualname__}(*{inp}) -> {outp}")
