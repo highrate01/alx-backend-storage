6873546f4657f0b2a1f66fc05a4ecdc825c7edc1
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-08-30 15:25:15 +0100

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Writing strings to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -25,3 +25,41 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None
+            ) -> Optional[Union[str, bytes, int, float]]:
+        """
+        Retrieve data from Redis using the given key and apply conversion func
+        Args:
+            key (str): The key to retrieve.
+            fn: A func to convert the data to desired format.
+        Returns:
+            Optional[Union[str, bytes, int, float]: The retrieved data.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve the value associated with the key and convert it to str.
+        Args:
+            key(str): the key to rerieve and convert
+        Returns:
+            Optional[str]: converted value as a string or none
+        """
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve the value associated with the key and convert it to str
+        Args:
+            key(str): the key to rerieve and convert
+        Returns:
+            Optional[int]: converted value as a int or none
+        """
+        return self.get(key, lambda x: int(x))
